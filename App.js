93e18f7ab0f7e49e71d14f26cd6dd8a1671e2b6f
Tiwat MB <tiwatposri@Tiwats-MacBook-Pro.local>
2025-09-22 23:37:22 +0700

    create

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text, View, Image ,ScrollView} from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 import { myStyle } from "./styles/mystyle";
 const logo = require("./assets/icon.png");
 export default function App() {
@@ -9,25 +9,58 @@ export default function App() {
     //fourth view is item
     <View style={myStyle.container}>
       <ScrollView>
-      <Image source = {logo} style={myStyle.image}></Image>
-      <Image source = {{uri: "https://picsum.photos/id/9/367/267"}} style={myStyle.image}></Image>
-      <Text style={myStyle.header}>Hello World!</Text>
-      <Text style={myStyle.content}>Developed By Jame</Text>
+        <Image source={logo} style={myStyle.image}></Image>
+        <Image
+          source={{ uri: "https://picsum.photos/id/9/367/267" }}
+          style={myStyle.image}
+        ></Image>
+        <Text style={myStyle.header}>Hello World!</Text>
+        <Text style={myStyle.content}>Developed By Jame</Text>
 
-      <Text style ={myStyle.content}>    at FlowParserMixin.parseStatementLike (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:2949:24)
-    at FlowParserMixin.parseStatementListItem (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12756:17)
-    at FlowParserMixin.parseBlockOrModuleBlockBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13325:61)
-    at FlowParserMixin.parseBlockBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13318:10)
-    at FlowParserMixin.parseBlock (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13306:10)
-    at FlowParserMixin.parseFunctionBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12110:24)  </Text>
+        <Text style={myStyle.content}>
+          {" "}
+          at FlowParserMixin.parseStatementLike (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:2949:24)
+          at FlowParserMixin.parseStatementListItem
+          (/Users/tiwatposri/Desktop/My Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12756:17)
+          at FlowParserMixin.parseBlockOrModuleBlockBody
+          (/Users/tiwatposri/Desktop/My Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13325:61)
+          at FlowParserMixin.parseBlockBody (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13318:10)
+          at FlowParserMixin.parseBlock (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13306:10)
+          at FlowParserMixin.parseFunctionBody (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12110:24){" "}
+        </Text>
 
-        <Text style ={myStyle.content}>    at FlowParserMixin.parseStatementLike (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:2949:24)
-    at FlowParserMixin.parseStatementListItem (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12756:17)
-    at FlowParserMixin.parseBlockOrModuleBlockBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13325:61)
-    at FlowParserMixin.parseBlockBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13318:10)
-    at FlowParserMixin.parseBlock (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13306:10)
-    at FlowParserMixin.parseFunctionBody (/Users/tiwatposri/Desktop/My Project/React Native Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12110:24)  </Text>
-     </ScrollView>
+        <Text style={myStyle.content}>
+          {" "}
+          at FlowParserMixin.parseStatementLike (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:2949:24)
+          at FlowParserMixin.parseStatementListItem
+          (/Users/tiwatposri/Desktop/My Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12756:17)
+          at FlowParserMixin.parseBlockOrModuleBlockBody
+          (/Users/tiwatposri/Desktop/My Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13325:61)
+          at FlowParserMixin.parseBlockBody (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13318:10)
+          at FlowParserMixin.parseBlock (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:13306:10)
+          at FlowParserMixin.parseFunctionBody (/Users/tiwatposri/Desktop/My
+          Project/React Native
+          Project/MyApplication/myapp/node_modules/@babel/parser/lib/index.js:12110:24){" "}
+        </Text>
+      </ScrollView>
       {/* <View
         style={{
           width: 80,
@@ -54,5 +87,3 @@ export default function App() {
     </View>
   );
 }
-
-
